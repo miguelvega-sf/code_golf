063a7f84b41c1b81deac854144fd15965a2890ed
Miguel Vega-Herrera <miguel.vegaherrera@sharefile.com>
2025-08-14 09:11:28 -0400

    attempt #3

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,28 +1,28 @@
 import sys
 
-m = [
-    (1000, "M"),
-    (900, "CM"),
-    (500, "D"),
-    (400, "CD"),
-    (100, "C"),
-    (90, "XC"),
-    (50, "L"),
-    (40, "XL"),
-    (10, "X"),
-    (9, "IX"),
-    (5, "V"),
-    (4, "IV"),
-    (1, "I")
-]
+m = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
 
 def r():
     for r in open(sys.argv[1]):
         o = ''
         n = int(r)
-        for v, l in m:
-            while n >= v:
-                n = n - v
-                o += l
+        for k in m:
+            while n >= k:
+                n = n - k
+                o += m[k]
         print(o)
 r()
